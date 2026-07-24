4c1623fe6f2f2e1b6428d6e8612f8bf4f73430b8
Ali Naghdi <AliNaghdi2006@gmail.com>
2026-07-24 14:43:07 +0330

    Add URL route for single post page
    - Add URL route for single post page
    - Map single post URL to its view
    - Configure URL name for single post route

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import index_view
+from blog.views import index_view, single_view
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index_view, name='index')
+    path('', index_view, name='index'),
+    path('single', single_view, name='single')
 ]
